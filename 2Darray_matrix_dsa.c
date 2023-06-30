f55ebeb32430faa3919f64c0b12c97541b556f40
sowjanyasambangi <137915153+sowjanyasambangi@users.noreply.github.com>
2023-06-30 21:43:54 +0530

    Add files via upload

---
+++ 2Darray_matrix_dsa.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+  int a[10][10],r,c,i,j;
+  printf("enter the number of rows and columns\n");
+  scanf("%d\n %d",&r,&c);
+  printf("enter values into matrix a\n");
+  for(i=0;i<r;i++)
+  {
+    for(j=0;j<c;j++)
+    {
+      scanf("%d",&a[i][j]);
+    }
+  }
+  printf("printing the matrix\n");
+  for(i=0;i<r;i++)
+  {
+    for(j=0;j<c;j++)
+    {
+      printf("%d\t",a[i][j]);
+    }
+    printf("\n");
+  }
+  getch();
+}
