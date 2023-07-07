9f1ca5b34138f71592359b704a0b40ec17b3b2d2
sowjanyasambangi <137915153+sowjanyasambangi@users.noreply.github.com>
2023-07-07 23:35:38 +0530

    Add files via upload

---
+++ string.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+      char s[100];
+      scanf("%[^\n]%*c", &s);
+      char str[]="Hello,World";
+      printf("%s",str);
+      printf("\n");
+      printf("%s",s);
+      return 0;
+
+
+
+
+
+}
